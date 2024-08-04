3091954224b1c04f286ae9f21a06276baf3cc272
Emrah Burak <emrhbrkky@gmail.com>
2024-08-04 16:05:59 +0300

    Discount microservice'in oluşturulması.

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //Tabloları veritabanına yansıtmak için kullanılır. Sonrasında bağlantı burdan kaldırılıp appsettings üzerinden devam edecek.
+        {
+            optionsBuilder.UseSqlServer("Server=LENOVO-PC\\SQLEXPRESS; initial Catalog=MultiShopDiscountDb; integrated Security=true;");
+        }
+        public DbSet<Coupon> Coupons { get; set; } //Veritabanına Coupons ismi ile yansıtacak.
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString); //CreateConnection çağırılınca yeni bir sql bağlantısı oluşturulacka.
+    }
+}
