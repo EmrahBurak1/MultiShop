814bc810508491c8d701da1f77c0acd16c76b9ec
Emrah Burak <emrhbrkky@gmail.com>
2024-08-19 22:11:04 +0300

    MediatR tasarım deseninin Ordering için kullanılması.

---
+++ Services/Order/Core/MultiShop.Order.Application/Services/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Services
+{
+    //SOLID'i ezmemek adına MediatR için registration işlemlerini doğrudan Program.cs içerisinde yapmadık. Onun yerine Application katmanına Services klasörü açıp onun içine tanımladık.
+    public static class ServiceRegistration //Static tanımladık diğer yerlerde buradaki methodlara doğrudan erişebilmek için.
+    {
+        public static void AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistration).Assembly)); //Bu şekilde ilgili kütüphanenin registration işlemi yapılmış olur.
+        }
+    }
+}
--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
 using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region
 builder.Services.AddScoped<GetAddressQueryHandler>(); //Constructor geçtiðimiz tüm sýnýflarý Dependency injection'a geçiyoruz.
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -16,6 +20,10 @@ builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+#endregion
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>)); //Repository için registration iþlemini yaptýk.
+builder.Services.AddApplicationService(builder.Configuration); //Burada da servisimizi eklemiþ oluyoruz. SOLID'i ezmemek için MediatR'ý application içindeki services klasörüne ekledik. 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
