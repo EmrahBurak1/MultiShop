41dcb53aa1fe45bcb40073b70a1d2c8e90e12ecc
Emrah Burak <emrhbrkky@gmail.com>
2024-09-04 21:35:09 +0300

    Cargo abstract ve concrete sınıfların tanımlanması.

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Concrete/CargoOperationManager.cs
@@ -0,0 +1,45 @@
+﻿using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Concrete
+{
+    public class CargoOperationManager : ICargoOperationService
+    {
+        private readonly ICargoOperationService _cargoOperationService;
+
+        public CargoOperationManager(ICargoOperationService cargoOperationService)
+        {
+            _cargoOperationService = cargoOperationService;
+        }
+
+        public void TDelete(int id)
+        {
+            _cargoOperationService.TDelete(id);
+        }
+
+        public List<CargoDetail> TGetAll()
+        {
+            return _cargoOperationService.TGetAll();
+        }
+
+        public CargoDetail TGetById(int id)
+        {
+            return _cargoOperationService.TGetById(id);
+        }
+
+        public void TInsert(CargoDetail entity)
+        {
+            _cargoOperationService.TInsert(entity);
+        }
+
+        public void TUpdate(CargoDetail entity)
+        {
+            _cargoOperationService.TUpdate(entity);
+        }
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/EntityFramework/EfCargoCompanyDal.cs
@@ -0,0 +1,21 @@
+﻿using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.Repositories;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.EntityFramework
+{
+    //Generic sınıfta yapılamayan entity'ye özgü methodlar entityframework klasörü içerisinde ayrı ayrı sınıflarda tanımlanabilir.
+    public class EfCargoCompanyDal : GenericRepository<CargoCompany>, ICargoCompanyDal 
+    {
+        public EfCargoCompanyDal(CargoContext context) : base(context) //EfCargoCompanyDal context sınıfını dahil etmemizi ister constructor olarak eklenip base olarakta context'i göndereceğimizi belirtiriz.
+        {
+                
+        }
+    }
+}
