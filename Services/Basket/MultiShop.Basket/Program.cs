3fd7bd8213ae0e694587b3f76cf45b00344f77b9
Emrah Burak Kaya <emrhbrkky@outlook.com>
2024-09-13 18:09:25 +0300

    Basket mikroservisi işlemleri. Redis için gerekli tanımlamalar.

--- Services/Basket/MultiShop.Basket/Program.cs
+++ Services/Basket/MultiShop.Basket/Program.cs
@@ -1,8 +1,42 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Options;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
+using MultiShop.Basket.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var requireAuthotizePolict = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build(); //Bu þekilde authorization iþlemi için kullanýcý bilgisinin zorunlu olduðu koþulunu belirledik.
 
-builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServer:Authority"];
+    opt.Audience = "ResourceBasket";
+    opt.RequireHttpsMetadata = false;
+});
+
+//Dependency iþlemleri yapýlýr.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+    var redis = new RedisService(redisSettings.Host, redisSettings.Port);
+    redis.Connect();
+    return redis;
+});//Redis ayarlarý bu þekilde yapýlýr.
+
+//Authorize iþleminde kullanýcý giriþine zorlanýr. Kullanýcý bilgisi yoksa hata verir.
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthotizePolict));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +52,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ Services/Basket/MultiShop.Basket/Settings/RedisService.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace MultiShop.Basket.Settings
+{
+    public class RedisService
+    {
+        public string _host { get; set; }
+        public int _port { get; set; }
+
+        private ConnectionMultiplexer _connectionMultiplexer;
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}"); //Connect methodu çağırıldığında host ve port numarasına erişim sğalayabileceğiz.
+
+        public IDatabase GetDb(int db = 1)=>_connectionMultiplexer.GetDatabase(0); //Getdb bize veritabanını getirir. db=1 dememimiz sebebi redise db ye baktığımız zaman karşımıza 16 tane db örneği çıkıyor bu dblerden her birini domain için kullanabiliyoruz örneğin development için product için test için farklı dbleri kullanabiliyoruz. 0.sırada olan database'i getir diyoruz.
+
+    }
+}
