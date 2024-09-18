366cbb3a65ef1af54077f6843bf236cca6f30888
Emrah Burak Kaya <emrhbrkky@outlook.com>
2024-09-18 17:43:14 +0300

    Temanın dahil edilip düzenlenmesi.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _HeadUILayoutComponentPartial: ViewComponent //Partial component kullanımı için ViewComponent sınıfından miras alınır.
+    {
+        public IViewComponentResult Invoke() //Invoke metodu ViewComponent sınıfından miras alınan metoddur.
+        {
+            return View(); //View() metodu ile ilgili view döndürülür.
+        }
+    }
+}
