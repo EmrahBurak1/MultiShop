64ef3c74a81decc1b66182c42847e58b0de5be3f
Emrah Burak Kaya <emrhbrkky@outlook.com>
2024-10-14 18:59:24 +0300

    Ürün ekleme işlemleri

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
@@ -32,6 +35,41 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync(); //Gelen veriyi json olarak okuyoruz.
                 var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData); //Json veriyi metin formatına çeviriyoruz.
                 return View(values);
+            }
+            return View();
+        }
+
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = "Ürün Listesi";
+            ViewBag.v0 = "Ürün İşlemleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            List<SelectListItem> categoryValues = (from x in values select new SelectListItem //Yeni bir liste oluşturuyoruz. Liste içerisi bu şekilde doldurulur.
+            {
+                Text = x.CategoryName, //Dropdown'da gözükecek olan değer.
+                Value = x.CategoryID
+            }).ToList();
+            ViewBag.CategoryValues = categoryValues; //View tarafında dropdownlist'i doldurabilmek için ViewBag'e atadık.
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            var client = _httpClientFactory.CreateClient(); //Parametre olarak alınan değeri değişkene atadık.
+            var jsonData = JsonConvert.SerializeObject(createProductDto); //Json formatına çevirdik.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); //Json formatındaki veriyi content olarak atadık. İkinci parametre hangi dil desteğinde olduğu. Üçüncü parametre ise mediatype'ın ne olduğunu belirtir. 
+            var responseMessage = await client.PostAsync("https://localhost:7070/api/Products", stringContent); //İlgili adrese istekte bulunabilmek için responseMessage değişkenine atadık.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" }); //İşlem başarılıysa Index sayfasına yönlendir. İkinci parametre controller adı, üçüncü parametre ise area adıdır.
             }
             return View();
         }
