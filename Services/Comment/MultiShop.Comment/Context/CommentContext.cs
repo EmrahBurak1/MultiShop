430f8361e40c2a98b8686bfc57f655f73c6cb5ba
Emrah Burak Kaya <emrhbrkky@outlook.com>
2024-11-24 22:15:41 +0300

    Yorumların admin sayfasında listelenmesi.

---
+++ Services/Comment/MultiShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1442; Initial Catalog=MultiShopCommentDb; User=sa; Password=123456aA*;"); //Docker db bilgilerini ekledik.
+        }
+
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
