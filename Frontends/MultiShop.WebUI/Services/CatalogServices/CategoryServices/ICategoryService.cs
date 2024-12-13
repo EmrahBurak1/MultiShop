a2f1ae30d7bfafdd05850028eedfb033d6eac191
Emrah Burak Kaya <emrhbrkky@outlook.com>
2024-12-13 16:41:22 +0300

    Client Credential Token Handler'ın oluşturulması.

---
+++ Frontends/MultiShop.WebUI/Services/CatalogServices/CategoryServices/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+
+namespace MultiShop.WebUI.Services.CatalogServices.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync(); //List olarak ResultCategoryDto'yu döndürecek.
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    }
+}
