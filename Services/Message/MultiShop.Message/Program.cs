cb5af6269bd6a49bb67ffa6bb175e90dc6d389cd
Emrah Burak Kaya <emrhbrkky@outlook.com>
2025-01-20 17:44:37 +0300

    Mesaj mikroservisi tamamlandı.

--- Services/Message/MultiShop.Message/Program.cs
+++ Services/Message/MultiShop.Message/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using MultiShop.Message.Dal.Context;
 using MultiShop.Message.Services;
@@ -6,6 +7,13 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "ResourceMessage";
+    opt.RequireHttpsMetadata = false; 
+});
+
 builder.Services.AddDbContext<MessageContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -30,6 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
